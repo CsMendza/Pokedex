24374fe23a5a2faed0056489d35ef738ba77a087
CsMndza <charlymj21@gmail.com>
2024-05-29 00:02:11 -0600

    Final commit

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const port = 3000;
 
 // Configurar conexión a la base de datos
 const db = mysql.createConnection({
-    host: 'pokedex.cbesw00i88cu.us-east-1.rds.amazonaws.com',
+    host: 'pokedex.cvx3mgu5vzra.us-east-1.rds.amazonaws.com',
     user: 'sebas',
     password: 'mcdonald',
     database: 'pokedex'
@@ -70,5 +70,5 @@ app.delete('/:id', (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`Server running at http://44.223.13.47:3000/`);
+    console.log(`Server running at http://18.234.112.235:3000/`);
 });
